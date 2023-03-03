75ab3d077c052bf8489fc059fbc4250594aea274
Hossein Fadaei <mehrdadfadaee4@gmail.com>
2023-03-03 12:33:21 +0330

    variable-in-php

--- PHP/syntax/index.php
+++ PHP/syntax/index.php
@@ -1,7 +1,37 @@
 <?php
 
-// Print hello world in php
+/* print hello world in php */
+
 echo "Hello World";
+
+
+/* variable in php */
+
+
+$FirstName="ArashFadaei";
+
+// echo $FirstName; => ArashFadaei
+
+
+$phone= 09140065379;
+
+// echo $phone; => 09140065379
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
