7e6f8fea3a61d93cb27515104332b67716d1983c
Hossein Fadaei <mehrdadfadaee4@gmail.com>
2023-03-03 12:48:09 +0330

    const-in-php

--- PHP/syntax/index.php
+++ PHP/syntax/index.php
@@ -19,6 +19,40 @@ $phone= 09140065379;
 
 
 
+/* const in php */
+
+const URL = "https://www.google.com";
+
+
+// echo URL ; // = >  https://www.google.com
+
+
+define("BASEURL","https://www.google.com");
+
+// echo BASEURL;=> https://www.google.com
+
+
+
+
+
+// define("BASEURL","https://www.google.com", true);
+
+// echo BASseurl; => https://www.google.com  because case_insensitive = true
+
+
+// echo URL; =>https://www.google.com
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
